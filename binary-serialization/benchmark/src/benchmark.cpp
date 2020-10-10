e8c5a78b56e61467ea97062701a775bab9b9f0df
Daniil Kovalev <kovdan01@yandex.ru>
2020-10-11 02:06:13 +0300

    Add binary <-> C++ object conversion benchmark

--- binary-serialization/benchmark/src/benchmark.cpp
+++ binary-serialization/benchmark/src/benchmark.cpp
@@ -38,6 +38,28 @@ static void msgpack_serialized_to_dom(bm::State& state)
 }
 BENCHMARK(msgpack_serialized_to_dom);
 
+static void msgpack_object_to_serialized(bm::State& state)
+{
+    for (auto _ : state)
+    {
+        msgpack::sbuffer packed;
+        msgpack::pack(packed, messages.messages());
+        bm::DoNotOptimize(packed);
+    }
+}
+BENCHMARK(msgpack_object_to_serialized);
+
+static void msgpack_serialized_to_object(bm::State& state)
+{
+    msgpack::sbuffer packed = messages.to_msgpack_buffer();
+    for (auto _ : state)
+    {
+        hw1::MessageVector unpacked(packed);
+        bm::DoNotOptimize(unpacked);
+    }
+}
+BENCHMARK(msgpack_serialized_to_object);
+
 static void cbor_dom_to_serialized(bm::State& state)
 {
     hw1::cbor::Item dom = messages.to_cbor_dom();
@@ -60,12 +82,40 @@ static void cbor_serialized_to_dom(bm::State& state)
 }
 BENCHMARK(cbor_serialized_to_dom);
 
+static void cbor_object_to_serialized(bm::State& state)
+{
+    for (auto _ : state)
+    {
+        hw1::cbor::Buffer packed = messages.to_cbor_buffer();
+        bm::DoNotOptimize(packed);
+    }
+}
+BENCHMARK(cbor_object_to_serialized);
+
+static void cbor_serialized_to_object(bm::State& state)
+{
+    hw1::cbor::Buffer packed = messages.to_cbor_buffer();
+    for (auto _ : state)
+    {
+        hw1::MessageVector unpacked(packed);
+        bm::DoNotOptimize(unpacked);
+    }
+}
+BENCHMARK(cbor_serialized_to_object);
+
 static void test()
 {
+    std::cout << "Performing tests...\n";
     const hw1::MessageVector& expected = messages;
     {
         msgpack::sbuffer sbuf = expected.to_msgpack_buffer();
         hw1::MessageVector got(sbuf);
+        assert(expected == got);
+    }
+    {
+        msgpack::sbuffer sbuf = expected.to_msgpack_buffer();
+        msgpack::object_handle oh = msgpack::unpack(sbuf.data(), sbuf.size());
+        hw1::MessageVector got(oh);
         assert(expected == got);
     }
     {
@@ -79,10 +129,17 @@ static void test()
         assert(expected == got);
     }
     {
+        hw1::cbor::Buffer buffer = expected.to_cbor_buffer();
+        hw1::cbor::Item item(buffer);
+        hw1::MessageVector got(item);
+        assert(expected == got);
+    }
+    {
         hw1::cbor::Item item = expected.to_cbor_dom();
         hw1::MessageVector got(item);
         assert(expected == got);
     }
+    std::cout << "Passed!\n";
 }
 
 int main(int argc, char** argv) try
@@ -121,13 +178,23 @@ int main(int argc, char** argv) try
     in_file.seekg(0, std::ios::beg);
 
     std::vector<char> buffer(static_cast<std::size_t>(size));
+    std::cout << "Reading data from file \"" << in_file_name << "\"..." << std::endl;
     if (!in_file.read(buffer.data(), size))
         throw std::runtime_error("Error while reading from file");
+    std::cout << "Read complete!\n"
+                 "Deserializing test data from binary to DOM..." << std::endl;
 
     msgpack::object_handle oh = msgpack::unpack(buffer.data(), buffer.size());
+    std::cout << "Deserialize complete!\n"
+              << "Converting from DOM to std::vector<hw1::Message>..." << std::endl;
     messages = hw1::MessageVector(oh);
+    std::cout << "Convert complete!" << std::endl;
 
+#ifndef NDEBUG
     test();
+#endif
+
+    std::cout << "Starting benchmarks..." << std::endl;
     bm::Initialize(&argc, argv);
     bm::RunSpecifiedBenchmarks();
 }
