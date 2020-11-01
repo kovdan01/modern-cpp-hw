a87dd76106249029661e7483be9955bf4bb2605d
Daniil Kovalev <kovdan01@yandex.ru>
2020-11-01 16:13:53 +0300

    Make hw1-wrappers library header-only
    All functions in this library are small and were explicitely marked inline.
    Might be useful when compiling and linking without `-flto=thin`.

--- hw1-binary-serialization/wrappers/include/bson_wrapper.hpp
+++ hw1-binary-serialization/wrappers/include/bson_wrapper.hpp
@@ -3,8 +3,6 @@
 
 #include <bson/bson.h>
 
-#include <hw1/wrappers/export.h>
-
 #include <array>
 #include <charconv>
 #include <memory>
@@ -15,98 +13,98 @@
 namespace hw1::bson
 {
 
-class HW1_WRAPPERS_EXPORT Base
+class Base
 {
 public:
-    Base(const Base&) noexcept = default;
-    Base& operator=(const Base&) noexcept = default;
-    Base(Base&&) noexcept = default;
-    Base& operator=(Base&&) noexcept = default;
+   Base(const Base&) noexcept = default;
+   Base& operator=(const Base&) noexcept = default;
+   Base(Base&&) noexcept = default;
+   Base& operator=(Base&&) noexcept = default;
 
-    ~Base();
+   inline ~Base();
 
-    [[nodiscard]] const bson_t* handle() const noexcept;
-    bson_t* handle() noexcept;
+   [[nodiscard]] inline const bson_t* handle() const noexcept;
+   [[nodiscard]] inline bson_t* handle() noexcept;
 
-    [[nodiscard]] const std::uint8_t* data() const noexcept;
-    std::uint8_t* data() noexcept;
+   [[nodiscard]] inline const std::uint8_t* data() const noexcept;
+   [[nodiscard]] inline std::uint8_t* data() noexcept;
 
-    [[nodiscard]] std::uint32_t size() const noexcept;
+   [[nodiscard]] inline std::uint32_t size() const noexcept;
 
-    void append_uint64(std::string_view key, std::uint64_t value);
-    void append_int64(std::string_view key, std::int64_t value);
-    void append_utf8(std::string_view key, std::string_view value);
-    void append_binary(std::string_view key, std::span<const std::uint8_t> value);
+   inline void append_uint64(std::string_view key, std::uint64_t value);
+   inline void append_int64(std::string_view key, std::int64_t value);
+   inline void append_utf8(std::string_view key, std::string_view value);
+   inline void append_binary(std::string_view key, std::span<const std::uint8_t> value);
 
 protected:
-    Base() noexcept = default;
-    Base(const bson_t&) noexcept;
+   Base() noexcept = default;
+   inline Base(const bson_t&) noexcept;
 
 private:
-    bson_t m_bson;
+   bson_t m_bson;
 };
 
 using Ptr = std::unique_ptr<Base>;
 
-class HW1_WRAPPERS_EXPORT Bson : public Base
+class Bson : public Base
 {
 public:
-    using Base::Base;
+   using Base::Base;
 
-    Bson() noexcept;
+   inline Bson() noexcept;
 };
 
-class HW1_WRAPPERS_EXPORT SubArray : public Base
+class SubArray : public Base
 {
 public:
-    using Base::Base;
+   using Base::Base;
 
-    SubArray(Base& parent, std::string_view key);
-    ~SubArray();
+   inline SubArray(Base& parent, std::string_view key);
+   inline ~SubArray();
 
-    void append_uint64(std::uint64_t value);
-    void append_int64(std::int64_t value);
-    void append_utf8(std::string_view value);
-    void append_binary(std::span<const std::uint8_t> value);
+   inline void append_uint64(std::uint64_t value);
+   inline void append_int64(std::int64_t value);
+   inline void append_utf8(std::string_view value);
+   inline void append_binary(std::span<const std::uint8_t> value);
 
-    void increment() noexcept;
-    [[nodiscard]] std::uint32_t index() const noexcept;
+   inline void increment() noexcept;
+   [[nodiscard]] inline std::uint32_t index() const noexcept;
 
 private:
-    Base& m_parent;
-    std::uint32_t m_index = 0;
+   Base& m_parent;
+   std::uint32_t m_index = 0;
 };
 
-class HW1_WRAPPERS_EXPORT Iter
+class Iter
 {
 public:
-    Iter(const bson_t* document);
+   inline Iter(const bson_t* document);
 
-    Iter(const Iter&) noexcept = default;
-    Iter& operator=(const Iter&) noexcept = default;
-    Iter(Iter&&) noexcept = default;
-    Iter& operator=(Iter&&) noexcept = default;
+   Iter(const Iter&) noexcept = default;
+   Iter& operator=(const Iter&) noexcept = default;
+   Iter(Iter&&) noexcept = default;
+   Iter& operator=(Iter&&) noexcept = default;
 
-    ~Iter() = default;
+   ~Iter() = default;
 
-    bool next() noexcept;
+   inline bool next() noexcept;
 
-    [[nodiscard]] bool is_uint64() const noexcept;
-    [[nodiscard]] bool is_int64() const noexcept;
-    [[nodiscard]] bool is_binary() const noexcept;
-    [[nodiscard]] bool is_utf8() const noexcept;
-    [[nodiscard]] bool is_array() const noexcept;
+   [[nodiscard]] inline bool is_uint64() const noexcept;
+   [[nodiscard]] inline bool is_int64() const noexcept;
+   [[nodiscard]] inline bool is_binary() const noexcept;
+   [[nodiscard]] inline bool is_utf8() const noexcept;
+   [[nodiscard]] inline bool is_array() const noexcept;
 
-    [[nodiscard]] std::uint64_t as_uint64() const noexcept;
-    [[nodiscard]] std::int64_t as_int64() const noexcept;
-    [[nodiscard]] std::span<const std::uint8_t> as_binary() const;
-    [[nodiscard]] std::string_view as_utf8() const;
-    [[nodiscard]] Iter as_array() const noexcept;
+   [[nodiscard]] inline std::uint64_t as_uint64() const noexcept;
+   [[nodiscard]] inline std::int64_t as_int64() const noexcept;
+   [[nodiscard]] inline std::span<const std::uint8_t> as_binary() const;
+   [[nodiscard]] inline std::string_view as_utf8() const;
+   [[nodiscard]] inline Iter as_array() const noexcept;
 
 private:
-    Iter() noexcept = default;
+   inline Iter() noexcept = default;
 
-    bson_iter_t m_iter;
+   bson_iter_t m_iter;
 };
 
 }  // namespace hw1::bson
@@ -115,15 +113,17 @@ private:
 #define HW1_BSON_DETAIL_UNIQUE_ID(varname, lineno) HW1_BSON_DETAIL_UNIQUE_ID_IMPL(varname, lineno)
 
 #define HW1_BSON_DECLARE_STRING_INDEX(string_name, sub_array)                               \
-    std::array<char, 11> HW1_BSON_DETAIL_UNIQUE_ID(str, __LINE__);                          \
-    auto [HW1_BSON_DETAIL_UNIQUE_ID(ptr, __LINE__),                                         \
-          HW1_BSON_DETAIL_UNIQUE_ID(ec, __LINE__)] =                                        \
-                  std::to_chars(HW1_BSON_DETAIL_UNIQUE_ID(str, __LINE__).data(),            \
-                                HW1_BSON_DETAIL_UNIQUE_ID(str, __LINE__).data() +           \
-                                HW1_BSON_DETAIL_UNIQUE_ID(str, __LINE__).size(),            \
-                                (sub_array).index());                                       \
-    assert(HW1_BSON_DETAIL_UNIQUE_ID(ec, __LINE__) == std::errc());                         \
-    std::string_view string_name(HW1_BSON_DETAIL_UNIQUE_ID(str, __LINE__).data(),           \
-                                 HW1_BSON_DETAIL_UNIQUE_ID(ptr, __LINE__));
+   std::array<char, 11> HW1_BSON_DETAIL_UNIQUE_ID(str, __LINE__);                          \
+   auto [HW1_BSON_DETAIL_UNIQUE_ID(ptr, __LINE__),                                         \
+         HW1_BSON_DETAIL_UNIQUE_ID(ec, __LINE__)] =                                        \
+                 std::to_chars(HW1_BSON_DETAIL_UNIQUE_ID(str, __LINE__).data(),            \
+                               HW1_BSON_DETAIL_UNIQUE_ID(str, __LINE__).data() +           \
+                               HW1_BSON_DETAIL_UNIQUE_ID(str, __LINE__).size(),            \
+                               (sub_array).index());                                       \
+   assert(HW1_BSON_DETAIL_UNIQUE_ID(ec, __LINE__) == std::errc());                         \
+   std::string_view string_name(HW1_BSON_DETAIL_UNIQUE_ID(str, __LINE__).data(),           \
+                                HW1_BSON_DETAIL_UNIQUE_ID(ptr, __LINE__));
 
+#include "bson_wrapper.inc"
+    
 #endif  // HW1_BINARY_SERIALIZATION_WRAPPERS_BSON_WRAPPER_HPP_
--- hw1-binary-serialization/wrappers/include/cbor_wrapper.hpp
+++ hw1-binary-serialization/wrappers/include/cbor_wrapper.hpp
@@ -3,29 +3,27 @@
 
 #include <cbor.h>
 
-#include <hw1/wrappers/export.h>
-
 #include <string>
 #include <vector>
 
 namespace hw1::cbor
 {
 
-class HW1_WRAPPERS_EXPORT Buffer
+class Buffer
 {
 public:
     Buffer() noexcept = default;
-    Buffer(const Buffer&);
-    Buffer& operator=(const Buffer&);
-    Buffer(Buffer&&) noexcept;
-    Buffer& operator=(Buffer&&) noexcept;
-    ~Buffer();
+    inline Buffer(const Buffer&);
+    inline Buffer& operator=(const Buffer&);
+    inline Buffer(Buffer&&) noexcept;
+    inline Buffer& operator=(Buffer&&) noexcept;
+    inline ~Buffer();
 
-    Buffer(const cbor_item_t* item);
+    inline Buffer(const cbor_item_t* item);
 
-    [[nodiscard]] std::size_t size() const noexcept;
-    [[nodiscard]] cbor_data data() const noexcept;
-    cbor_mutable_data data() noexcept;
+    [[nodiscard]] inline std::size_t size() const noexcept;
+    [[nodiscard]] inline cbor_data data() const noexcept;
+    [[nodiscard]] inline cbor_mutable_data data() noexcept;
 
 private:
     std::size_t m_size = 0;
@@ -35,48 +33,50 @@ private:
     void dtor_impl() noexcept;
 };
 
-class HW1_WRAPPERS_EXPORT Item
+class Item
 {
 public:
-    explicit Item(cbor_item_t* item) noexcept;
-    Item(const Buffer& buffer);
-    Item(const Item&) noexcept;
-    Item& operator=(const Item&) noexcept;
-    Item(Item&&) noexcept;
-    Item& operator=(Item&&) noexcept;
+    explicit inline Item(cbor_item_t* item) noexcept;
+    inline Item(const Buffer& buffer);
+    inline Item(const Item&) noexcept;
+    inline Item& operator=(const Item&) noexcept;
+    inline Item(Item&&) noexcept;
+    inline Item& operator=(Item&&) noexcept;
 
-    ~Item();
+    inline ~Item();
 
-    operator const cbor_item_t*() const noexcept;
-    operator cbor_item_t*() noexcept;
+    inline operator const cbor_item_t*() const noexcept;
+    inline operator cbor_item_t*() noexcept;
 
-    void array_push(cbor_item_t* item);
+    inline void array_push(cbor_item_t* item);
 
 private:
     cbor_item_t* m_item = nullptr;
 
-    void dtor_impl();
+    inline void dtor_impl();
 };
 
-HW1_WRAPPERS_EXPORT Item build_uint8(std::uint8_t value);
-HW1_WRAPPERS_EXPORT Item build_uint16(std::uint16_t value);
-HW1_WRAPPERS_EXPORT Item build_uint32(std::uint32_t value);
-HW1_WRAPPERS_EXPORT Item build_uint64(std::uint64_t value);
-HW1_WRAPPERS_EXPORT Item build_negint8(std::uint8_t value);
-HW1_WRAPPERS_EXPORT Item build_negint16(std::uint16_t value);
-HW1_WRAPPERS_EXPORT Item build_negint32(std::uint32_t value);
-HW1_WRAPPERS_EXPORT Item build_negint64(std::uint64_t value);
+inline Item build_uint8(std::uint8_t value);
+inline Item build_uint16(std::uint16_t value);
+inline Item build_uint32(std::uint32_t value);
+inline Item build_uint64(std::uint64_t value);
+inline Item build_negint8(std::uint8_t value);
+inline Item build_negint16(std::uint16_t value);
+inline Item build_negint32(std::uint32_t value);
+inline Item build_negint64(std::uint64_t value);
 
-HW1_WRAPPERS_EXPORT Item new_definite_array(std::size_t size);
-HW1_WRAPPERS_EXPORT Item new_indefinite_array();
+inline Item new_definite_array(std::size_t size);
+inline Item new_indefinite_array();
 
-HW1_WRAPPERS_EXPORT Item build_string(const std::string& str);
-HW1_WRAPPERS_EXPORT Item build_string(std::string_view str);
-HW1_WRAPPERS_EXPORT Item build_string(const char* val);
-HW1_WRAPPERS_EXPORT Item build_string(const char* val, std::size_t length);
+inline Item build_string(const std::string& str);
+inline Item build_string(std::string_view str);
+inline Item build_string(const char* val);
+inline Item build_string(const char* val, std::size_t length);
 
-HW1_WRAPPERS_EXPORT Item build_bytestring(cbor_data handle, std::size_t length);
+inline Item build_bytestring(cbor_data handle, std::size_t length);
 
 }  // namespace hw1::cbor
+
+#include "cbor_wrapper.inc"
 
 #endif  // HW1_BINARY_SERIALIZATION_WRAPPERS_CBOR_WRAPPER_HPP_
